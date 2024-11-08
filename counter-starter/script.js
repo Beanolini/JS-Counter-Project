f339f8fcca30c104660176d5b82a991c2177ad14
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2024-11-08 16:33:42 +0800

    added input field

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -2,8 +2,8 @@ const minus = (step) => {
   // We gave the number element an id of number so we can use that to get the element
   let numberElement = document.getElementById("number");
   // We can use the innerText property to get the text inside the element
-  let number = numberElement.innerText;
-  number = parseInt(number) + step;
+  let number = parseInt(numberElement.innerText);
+  number += step;
   numberElement.innerText = number;
 }; // Close the minus function here
 
@@ -11,4 +11,20 @@ const minus = (step) => {
 function reset() {
   // Set the text content of the number element to "0"
   document.getElementById("number").innerText = "0";
+}
+function customChange() {
+  let numberElement = document.getElementById("number");
+  let customStepElement = document.getElementById("custom-step");
+
+  let customStep = parseInt(customStepElement.value);
+
+  if (!isNaN(customStep)) {
+    let number = parseInt(numberElement.innerText);
+    number += customStep;
+    numberElement.innerText = number;
+
+    customStepElement.value = "";
+  } else {
+    alert("Please enter a valid number.");
+  }
 }
